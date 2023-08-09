ec044378f90a7f894503164f8a19b1657adc8707
behzad <90694618+behzadiv@users.noreply.github.com>
2023-08-09 11:14:29 +0330

    create initial files of Note-App

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+function App() {
+  return (
+    <div className="container">
+      <div className="note-header"></div>
+      <div className="note-app">
+        <div className="add-new-note"></div>
+        <div className="note-container"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
